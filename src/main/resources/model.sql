374f65ae40fcfd5a29e628e8bda41175abfa00b0
superbool <kefei@baixing.com>
2016-11-04 18:02:35 +0800

    add time

--- src/main/resources/model.sql
+++ src/main/resources/model.sql
@@ -1,9 +1,11 @@
 CREATE TABLE user_info (
   id         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  card_id    TEXT NOT NULL,
-  name       TEXT NOT NULL,
-  department TEXT NOT NULL,
-  same_id    TEXT DEFAULT NULL
+  card_id    TEXT                              NOT NULL,
+  name       TEXT                              NOT NULL,
+  department TEXT                              NOT NULL,
+  same_id    TEXT DEFAULT NULL,
+  create_at  INTEGER                           NOT NULL,
+  modify_at  INTEGER                           NOT NULL
 );
 CREATE UNIQUE INDEX idx_card_depart
   ON user_info (card_id, name, department);
