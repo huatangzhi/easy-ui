1d2d453d99928b0e2c2d4b38c7cae6fd18da407f
superbool <kefei@baixing.com>
2016-11-04 18:28:57 +0800

    update time

--- src/main/resources/model.sql
+++ src/main/resources/model.sql
@@ -3,9 +3,9 @@ CREATE TABLE user_info (
   card_id    TEXT                              NOT NULL,
   name       TEXT                              NOT NULL,
   department TEXT                              NOT NULL,
-  same_id    TEXT DEFAULT NULL,
-  create_at  INTEGER                           NOT NULL,
-  modify_at  INTEGER                           NOT NULL
+  same_id    TEXT      DEFAULT NULL,
+  create_at  TIMESTAMP DEFAULT (DATETIME('now', 'localtime')),
+  modify_at  TIMESTAMP DEFAULT (DATETIME('now', 'localtime'))
 );
 CREATE UNIQUE INDEX idx_card_depart
   ON user_info (card_id, name, department);
